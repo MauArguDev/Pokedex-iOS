4b7c1a05d64689b880a8bd02a2d1045406f091b6
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2022-10-01 16:10:58 -0600

    First commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Mauricio Argumedo on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
